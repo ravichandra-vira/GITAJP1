23c14a444dc2819a206584b29834c6b11dec3cc5
ravichandra-vira <namanivira@gmail.com>
2026-02-18 15:23:04 +0530

    test

---
+++ Test4.java
@@ -0,0 +1,13 @@
+class Data<T extends Number> {
+	T value;
+}
+
+public class Test4{
+	public static void main(String [] args){
+		Data<Integer> d1 =new Data<>();
+		Data<Double> d2 =new Data<>();
+		//Data<String> d3 =new Data<>();
+		
+		
+	}
+}
